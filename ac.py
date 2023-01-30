bc4f80baf0c1f5617e047f5cf9bcb0462c4eb000
KuyaAC <aciesnet02@gmail.com>
2023-01-30 16:56:19 +0800

    Syntax Analyzer

--- ac.py
+++ ac.py
@@ -1,18 +1,3 @@
-with open("DEV.ecpp", "r") as input_file:
-    # read the contents of the file
-    contents = input_file.read()
+import nltk
 
-# use regex to find all quoted text
-matches = re.findall(r'"([^"]*)"', contents)
-
-# replace the quoted text with the unquoted text + "4"
-for match in matches:
-    if "const" in match:
-        contents = contents.replace('"' + match + '"', match + "3")
-    else :
-        contents = contents.replace('"' + match + '"', match + "4")
-
-# open the output file
-with open("DEV2.ecpp", "w") as output_file:
-    # write the modified contents to the output file
-    output_file.write(contents)
+nltk.download()
